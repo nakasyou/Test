de82ccf81a46c3a7961e7e25badf6d0c5f933c4b
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-29 20:21:52 +0900

    add element

--- blagin.config.ts
+++ blagin.config.ts
@@ -13,6 +13,11 @@ export default {
       icon: "https://cdn.jsdelivr.net/npm/simple-icons@8.8.0/icons/twitter.svg",
       name: "Twitter",
     },
+    {
+      url: "https://matrix.to/#/@nakasyou:matrix.org",
+      icon: "https://cdn.jsdelivr.net/npm/simple-icons@8.8.0/icons/element.svg",
+      name: "Element",
+    },
   ],
-  desc: `プログラミングが大好きな中学生。東方Projectが好き。`
+  desc: `プログラミングが大好きな中学1年生。東方Projectが好き。`
 }
