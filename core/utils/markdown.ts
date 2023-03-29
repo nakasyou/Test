da59825216313ceee6856465f36b485b34843bf1
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-29 20:12:26 +0900

    fix

--- core/utils/markdown.ts
+++ core/utils/markdown.ts
@@ -5,11 +5,10 @@ import { parse } from "https://deno.land/std/encoding/yaml.ts";
 
 export function getYml(md: string): Record<string, any> {
   const lines = md.split("\n");
-  console.log(lines[0])
+  console.log([lines[0]])
   console.log(lines[0]!=="---")
   if (lines[0] !== "---") return {};
   const index = lines.slice(1).indexOf("---") + 1;
-  console.log(index)
   if (index === 0) return {};
   return {
     data: parse(lines.slice(1, index).join("\n")),
