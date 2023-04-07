9674d59755c6cc92d7ba90334416fb4fcfb8078a
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-07 21:04:02 +0900

    Update layout.tsx

--- core/layout.tsx
+++ core/layout.tsx
@@ -11,10 +11,8 @@ export default async function(children, {title}){
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="google-site-verification" content="ACaZWde-uW06btqxdWzZvgqHUw2T-27zvr8L89SI2z8" />
+        <link rel="stylesheet" href="/style.css"/>
         <Head/>
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <link rel="stylesheet"
@@ -23,7 +21,6 @@ export default async function(children, {title}){
         <script>hljs.initHighlightingOnLoad();</script>
         
         <title>{title}</title>
-        <link rel="stylesheet" href="/style.css"/>
       </head>
       <body>
         <div style="position:fixed;width:100%;">
