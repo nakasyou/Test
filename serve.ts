da45e6e6592ae06063615d9a8cdfbc32ed47e6d3
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-07 20:06:26 +0900

    Update serve.ts

--- serve.ts
+++ serve.ts
@@ -1,4 +1,5 @@
-import { Hono } from "https://deno.land/x/hono@v3.1.3/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v3.1.5/mod.ts";
+import { serveStatic } from "https://deno.land/x/hono@v3.1.5/middleware.ts";
 import { serve } from 'https://deno.land/std/http/server.ts';
 import indexpage from "./core/indexpage.tsx";
 import postpage from "./core/postpage.tsx";
@@ -14,5 +15,7 @@ app.get('/style.css', async c=>{
 app.get('/posts/:name',async c=>{
   return c.html(await postpage(c));
 })
-
+app.use('/*',serveStatic({
+  root: "./static"
+}))
 serve(app.fetch)
