0ae8ff75fac7edb0a5f67d9f8f66b4e7c35c185d
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-07 20:23:59 +0900

    Update layout.tsx

--- core/layout.tsx
+++ core/layout.tsx
@@ -13,8 +13,6 @@ export default function(children, {title}){
         <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
         <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon-152x152.png">
-        <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <link rel="stylesheet"
