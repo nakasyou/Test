ba4a9ea7b919d479120ab88b4784f8dd7bbc167d
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-07 17:09:29 +0900

    Update header.tsx

--- core/header.tsx
+++ core/header.tsx
@@ -4,7 +4,7 @@ import { Fragment, h, renderToString } from "https://deno.land/x/jsx/mod.ts"
 import config from "../blagin.config.ts";
 
 export default function(){
-  return <div class="header">
+  return <div class="header" style="height:30px">
     <div class="center">
 
     </div>
