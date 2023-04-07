99aa7528b5b6c814f4157b893aedb597b9e8936b
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-07 20:42:59 +0900

    add html lang

--- core/layout.tsx
+++ core/layout.tsx
@@ -6,7 +6,7 @@ import Head from "./head.tsx";
 
 export default function(children, {title}){
   return (<>
-    <html>
+    <html lang="ja">
       <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
