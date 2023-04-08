d9102b81e0c4d6bc6e813b2939d8e7e867cce032
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-08 17:28:37 +0900

    Update sitemap.ts

--- core/sitemap.ts
+++ core/sitemap.ts
@@ -2,7 +2,11 @@ import { getPostDatas } from "./indexpage.tsx";
 import { Sitemap } from "https://deno.land/x/deno_sitemap@0.1.2/mod.ts";
 
 export default async function(){
-  const datas = await getPostDatas();
   const sitemap = new Sitemap("https://nakasyou.deno.dev");
+  // posts
+  for(const post of (await getPostDatas())){
+    sitemap.add(`/posts/${post.name}`)
+  }
+  
   return sitemap.sitemap;
 }
