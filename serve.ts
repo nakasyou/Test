1ed97380697d6a9c8b994e4b9038b509517fd9d0
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-08 17:06:04 +0900

    Update serve.ts

--- serve.ts
+++ serve.ts
@@ -7,7 +7,11 @@ import postpage from "./core/postpage.tsx";
 const app = new Hono();
 
 app.get('/',async c=>{
-  return c.html(await indexpage());
+  try{
+    return c.html(await indexpage());
+  }catch(e){
+    return c.json({name:e.name,msg:e.message})
+  }
 })
 app.get('/style.css', async c=>{
   return c.body(await Deno.readTextFile("./style.css"))
