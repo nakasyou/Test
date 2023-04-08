371ef51b11636fbc2ea64bee9173aaf972d5378c
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-08 17:16:50 +0900

    Update serve.ts

--- serve.ts
+++ serve.ts
@@ -3,9 +3,13 @@ import { serveStatic } from "https://deno.land/x/hono@v3.1.5/middleware.ts";
 import { serve } from 'https://deno.land/std/http/server.ts';
 import indexpage from "./core/indexpage.tsx";
 import postpage from "./core/postpage.tsx";
+import sitemap from "./core/sitemap.ts";
 
 const app = new Hono();
 
+app.get('/sitemap.xml',async c=>{
+  return c.body(sitemap());
+})
 app.get('/',async c=>{
   try{
     return c.html(await indexpage());
@@ -22,4 +26,5 @@ app.get('/posts/:name',async c=>{
 app.use('/*',serveStatic({
   root: "./static"
 }))
+
 serve(app.fetch)
