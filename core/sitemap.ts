2425201a043a631b1a53516ea67c29160e31f970
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-08 17:33:49 +0900

    add root url

--- core/sitemap.ts
+++ core/sitemap.ts
@@ -3,6 +3,7 @@ import { Sitemap } from "https://deno.land/x/deno_sitemap@0.1.2/mod.ts";
 
 export default async function(){
   const sitemap = new Sitemap("https://nakasyou.deno.dev");
+  sitemap.add('/') // root
   // posts
   for(const post of (await getPostDatas())){
     sitemap.add(`/posts/${post.name}`)
