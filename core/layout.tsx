93aac658ca1158fbded8e2f8eeec84fffb87cc2f
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-08 12:03:25 +0900

    Update layout.tsx

--- core/layout.tsx
+++ core/layout.tsx
@@ -13,7 +13,6 @@ export default async function(children, {title,description}){
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="description" content={description}/>
         <meta name="google-site-verification" content="ACaZWde-uW06btqxdWzZvgqHUw2T-27zvr8L89SI2z8" />
-        <link rel="stylesheet" href="/style.css"/>
         <Head/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
