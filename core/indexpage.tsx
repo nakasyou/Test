ef479893d74a8a45dab1ae4e108968707de1424a
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-08 12:53:24 +0900

    Update indexpage.tsx

--- core/indexpage.tsx
+++ core/indexpage.tsx
@@ -46,7 +46,7 @@ export default async function(){
               style="padding: 24px 0px;"
               >{
               post.title
-            }</a>
+              }</a><br><span>{post.desc}</span>
           </li>
         })
       }
